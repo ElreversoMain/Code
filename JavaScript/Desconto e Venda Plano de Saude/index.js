68cb377624a18a215f5116396babcbdcb16d3ea8
Felipe Oliveira <felipe123oliveirasantos@gmail.com>
2022-05-12 21:45:29 -0300

    PlanoSaude

---
+++ JavaScript/Desconto e Venda Plano de Saude/index.js
@@ -0,0 +1,36 @@
+let pergunta=confirm("Você deseja ser atendido  ?")
+  if (confirm("Deseja Continuar sim ou não ?")){
+      console.log(confirm)
+
+ 
+let idade =Number(prompt("digite sua idade para darmos procedimento  "))
+if (idade <18){
+    console.log(`sua idade é ${idade} então , não associado paga R$: 106,34 associados R$ 74,44`)
+} else if (idade<23) {
+    console.log(`sua idade é ${idade} então , não associado paga R$ 123,25 , associados R$ 86,23`)
+
+} else if (idade <28){
+    console.log(`sua idade é ${idade} então , não associado paga R$ 137,87 , associados R$ 96,49`)
+
+} else if (idade<33) {
+    console.log(` sua idade é ${idade} então , não associado paga R$ 165,41  , associado R$ 115,79`)
+} else if (idade<38){
+    console.log(`sua idade é ${idade} então , não associado paga R$ 178,64 , associado R$ 125,05`)
+
+}else if (idade<43){
+   console.log(`sua idade é ${idade} então , não associado paga R$204,89 , associado R$ 143,42`)
+} else if(idade <48) {
+    console.log(`sua idade é ${idade} então , não associado paga R$ 260,63 , associado R$ 182,44`)
+
+} else if (idade <53) {
+    console.log(`sua idade é ${idade} então , não associado paga R$333,59 , associado R$ 233,51`)
+
+} else if (idade <58){ 
+    console.log(`sua idade é ${idade} então , não associado paga R$457,03 , associado R$ 319,92`)
+
+} else if ( idade< 100)
+    console.log(`sua idade é ${idade} então , não associado paga R$621,01  não associado R$ 434,71`)
+
+
+  } else 
+  alert("agradeço sua colaboração tenha uma boa tarde ") 
