026b0769ad2d2608938fb1a4a1b84fe31a4c07bf
Felipe Oliveira <felipe123oliveirasantos@gmail.com>
2022-05-17 23:23:20 -0300

    Nacionalidade

---
+++ JavaScript/PaisEnacionalidade/index.js
@@ -0,0 +1,33 @@
+const paisDeOrigem=prompt("qual pais você e ?")
+switch(paisDeOrigem){
+    case 'EUA':
+        console.log('Norte Americano')
+        break
+        case'Europa':
+        console.log('Europeu')
+        break
+        case'Inglaterra':
+        console.log('Inglês')
+        break
+        case'Canada':
+        console.log('Canadense')
+        break
+        case'China':
+        console.log('CHINESE')
+        break
+        case'França ':
+        console.log('Francês')
+        break
+        case'Alemanha':
+        console.log("Alemão")
+        break
+        case'Italia':
+        console.log("Italiano")
+        break
+        case"Brasil":
+        console.log("Brasileiro")
+        break
+        
+    }
+    
+
