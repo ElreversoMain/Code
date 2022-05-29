2517ef11345a026ed3ab58b40ae3c3d26f4ae9f8
ELreversoMain <felipe123oliveirasantos@gmail.com>
2022-05-29 17:11:40 -0300

    Whatzap

---
+++ React/whatzap/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Whatzap from "./Componente/Whatzap";
+
+class App extends React.Component {
+  render () {
+    return (
+     <Whatzap/>
+    )
+  }
+}
+export default App;
